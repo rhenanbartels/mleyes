72954dfe87d5c949b44a3a1017936f7dc3109abf
Rhenan Bartels <rhenan.bartels@gmail.com>
2020-05-26 11:44:17 -0300

    Refactor model's code

--- models/core.py
+++ models/core.py
@@ -1,64 +1,35 @@
-# author: Arun Ponnusamy
-# website: https://www.arunponnusamy.com
+# Inspired by https://github.com/arunponnusamy/cvlib
 
-# import necessary packages
 import cv2
 import numpy as np
-import os
-from pkg_resources import resource_filename, Requirement
 
-is_initialized = False
-prototxt = None
-caffemodel = None
-net = None
+
+prototxt = "models/deploy.protoxt"
+caffemodel = "models/res10_300x300_ssd_iter_140000.caffemodel"
+NET = cv2.dnn.readNetFromCaffe(prototxt, caffemodel)
+
 
 def detect_face(image, threshold=0.5):
-    if image is None:
-        return None
-
-    global is_initialized
-    global prototxt
-    global caffemodel
-    global net
-    if not is_initialized:
-
-        # access resource files inside package
-        # read pre-trained wieights
-        prototxt = "models/deploy.protoxt"
-        caffemodel = "models/res10_300x300_ssd_iter_140000.caffemodel"
-        net = cv2.dnn.readNetFromCaffe(prototxt, caffemodel)
-
-        is_initialized = True
-
-    # enable GPU if requested
-
     (h, w) = image.shape[:2]
 
-    # preprocessing input image
     blob = cv2.dnn.blobFromImage(image, 1.0, (300,300), (104.0,177.0,123.0))
-    net.setInput(blob)
+    NET.setInput(blob)
 
-    # apply face detection
-    detections = net.forward()
+    detections = NET.forward()
 
     faces = []
     confidences = []
 
-    # loop through detected faces
     for i in range(0, detections.shape[2]):
         conf = detections[0,0,i,2]
 
-        # ignore detections with low confidence
         if conf < threshold:
             continue
 
-        # get corner points of face rectangle
         box = detections[0,0,i,3:7] * np.array([w,h,w,h])
         (startX, startY, endX, endY) = box.astype('int')
 
         faces.append([startX, startY, endX, endY])
         confidences.append(conf)
 
-    # return all detected faces and
-    # corresponding confidences    
     return faces, confidences
